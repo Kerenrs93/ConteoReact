78372cad5ea86b5ed01cbd7fb348dcafddad1015
Kerenrs93 <kerenrs93@gmail.com>
2021-08-03 11:22:06 -0600

    Agregando proyecto de conteo terminado

---
+++ src/Contador.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const Contador=({valor})=>{
+
+    const [conteo, setConteo] = useState(valor)
+    const sumar=()=>{
+        setConteo(conteo+1);
+    }
+
+    const restar=()=>{
+        setConteo(conteo-1);
+    }
+
+    const reset=()=>{
+        setConteo(valor);
+    }
+
+    return(
+        <>
+            <h1>Contador</h1>
+            <h2>{conteo}</h2>
+            <button onClick={sumar}>+</button>
+            <button onClick={reset}>reset</button>
+            <button onClick={restar}>-</button>
+            
+        </>
+    )
+}
+export default Contador;
